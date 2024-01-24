98a899415549064c264a7dbb5cb4c5595ade3e73
Christopher Marsh-Bourdon <christopher@boggybumblebee.com>
2024-01-24 11:05:33 +0000

    Fleshed out department-service

---
+++ department-service/src/main/java/com/boggybumblebee/springboot/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,18 @@
+package com.boggybumblebee.springboot.departmentservice.client;
+
+
+import com.boggybumblebee.springboot.departmentservice.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+import java.util.List;
+
+@HttpExchange
+public interface EmployeeClient {
+
+    @GetExchange("/employee/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+
+}
---
+++ department-service/src/main/java/com/boggybumblebee/springboot/departmentservice/model/Employee.java
@@ -0,0 +1,4 @@
+package com.boggybumblebee.springboot.departmentservice.model;
+
+public record Employee(Long id, Long departmentId, String name, int age, String position) {
+}
