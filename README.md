25c825e9a3d00db3bf345cb92ae1a6da9e25132d
Christopher Marsh-Bourdon <christopher@boggybumblebee.com>
2024-01-25 14:58:28 +0000

    Added AOP Logging

--- README.md
+++ README.md
@@ -7,5 +7,18 @@ we take this further by adding in:
 - Maven Project Hierarchy - Done
 - JPA Repositories - Done
 - Authentication and Authorization - TODO
-- AOP Logging - TODO
+- AOP Logging - Done
 - ...
+
+## Running the Application
+Please ensure Docker or Docker Desktop is running on the instance, and then run the following command line:
+
+> docker run -d -p 9411:9411 openzipkin/zipkin
+
+Then start the modules in the following order:
+
+1. config-server
+2. service-registry
+3. employee-service
+4. department-service
+5. api-gateway
--- employee-service/src/main/java/com/boggybumblebee/springboot/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/boggybumblebee/springboot/employeeservice/controller/EmployeeController.java
@@ -2,8 +2,6 @@ package com.boggybumblebee.springboot.employeeservice.controller;
 
 import com.boggybumblebee.springboot.employeeservice.model.Employee;
 import com.boggybumblebee.springboot.employeeservice.repository.EmployeeRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +16,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 
     private final EmployeeRepository repository;
 
@@ -44,25 +41,21 @@ public class EmployeeController {
 
     @PostMapping
     public Employee add(@RequestBody Employee employee) {
-        LOGGER.info("Employee add: {}", employee);
         return repository.save(employee);
     }
 
     @GetMapping
     public List<Employee> findAll() {
-        LOGGER.info("Employee find");
         return repository.findAll();
     }
 
     @GetMapping("/{id}")
     public Employee findById(@PathVariable("id") Long id) {
-        LOGGER.info("Employee find: id={}", id);
         return repository.findById(id).orElseThrow();
     }
 
     @GetMapping("/department/{departmentId}")
     public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
-        LOGGER.info("Employee find: departmentId={}", departmentId);
         return repository.findByDepartmentId(departmentId);
     }
 
